4875b6ca9fbcca1612e2622493ef22ec587592b6
David Isles <diisles@yahoo.com>
2017-02-22 16:53:56 -0400

    changed for exponent

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,7 @@
 // Import a library to help create a component
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
+import Exponent from 'exponent';
 import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
@@ -13,5 +14,8 @@ const App = () => (
   );
 
 
-// Render it to the device
-AppRegistry.registerComponent('albums', () => App);
+// Render it to the device using Exponent
+Exponent.registerRootComponent('albums', () => App);
+
+
+// Used to Render to device when Not using Exponent.  AppRegistry.registerComponent('albums', () => App);
